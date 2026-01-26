b21dedfcc1e044f33fa3a2e5c2c8a3272d3044e1
keneth ornos <ornoskeneth@gmail.com>
2026-01-26 15:22:19 +0000

    can you fix that

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:25018529734:web:7889cb7706597010e224c7",
   "apiKey": "AIzaSyDIoR4K0ZJJxzYWMyq3nque7dZ0UtD8jGY",
   "authDomain": "studio-2070683605-69858.firebaseapp.com",
+  "storageBucket": "studio-2070683605-69858.appspot.com",
   "measurementId": "",
   "messagingSenderId": "25018529734"
 };
