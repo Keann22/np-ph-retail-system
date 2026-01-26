b0f4a0c8e6cfb365f84aa6405f0adc811e7f1411
keneth ornos <ornoskeneth@gmail.com>
2026-01-26 14:51:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2070683605-69858",
+  "appId": "1:25018529734:web:7889cb7706597010e224c7",
+  "apiKey": "AIzaSyDIoR4K0ZJJxzYWMyq3nque7dZ0UtD8jGY",
+  "authDomain": "studio-2070683605-69858.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "25018529734"
+};
